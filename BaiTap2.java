77f1802d6e3cdf41da1aa1f544d6f7346e372d10
kieuducchung <kdc311005@gmail.com>
2024-11-03 18:50:20 +0700

    Assigments2

---
+++ BaiTap2.java
@@ -0,0 +1,15 @@
+
+package baitap2;
+import java.util.Scanner;
+
+public class BaiTap2 {
+
+   
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter is string: ");
+            System.out.println("string data type :" + scanner.hasNext());
+       
+    }
+    
+}
---
+++ BaiTap3.java
@@ -0,0 +1,15 @@
+
+package baitap3;
+import java.util.Scanner;
+
+public class BaiTap3 {
+
+   
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter Integer: ");
+            System.out.println("integer? :" + scanner.hasNextInt());
+       
+    }
+    
+}
